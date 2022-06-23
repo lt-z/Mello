c37c05a2209ce4deb37c08cd95bbe44a51464808
Sam Harreschou <samharreschou2@gmail.com>
2022-06-23 09:26:34 -0700

    Add action functionality to card

--- server/controllers/cardsController.js
+++ server/controllers/cardsController.js
@@ -42,23 +42,60 @@ const getCard = async (req, res, next) => {
 const updateCard = async (req, res, next) => {
   try {
     const { dueDate, completed, listId, archived, ...remaining } = req.body.card;
-    console.log(req.body.card);
-    console.log(remaining);
 
-    const newAction = new Action({
-      ...req.body.card,
-      cardId: req.params.id,
-    });
-    console.log(remaining);
-
-    const savedAction = await newAction.save();
     const card = await Card.findById(req.params.id);
-    card.actions = card.actions.concat(savedAction)
+    console.log('Card', card);
+    const actionList = action(dueDate, completed, listId, archived, card);
+    actionList.forEach(async (action) => {
+      const newAction = new Action({
+        description: action,
+        cardId: req.params.id,
+      });
+      const savedAction = await newAction.save();
+      card.actions = card.actions.concat(savedAction)
+      await card.save();
+    })
+    card = {...card, ...req.body.card};
+    console.log(card);
+    console.log('req.body.card:')
+    console.log(req.body.card)
     await card.save();
-
+    console.log('RESPONSE', card);
     res.json(card);
   } catch(e) {
     new HttpError('Updating card failed, please try again: ' + e, 500)
+  }
+}
+
+const action = (dueDate, completed, listId, archived, card) => {
+  let actionList = []
+
+  if (dueDate && card.dueDate === null) { 
+    actionList.push('Added a due date')
+  }
+  if (dueDate !== undefined && dueDate === null && card.dueDate) {
+    actionList.push('Removed the due date')
+  }
+  if (dueDate !== undefined && dueDate !== card.dueDate) {
+    actionList.push('Due date changed')
+  }
+
+  if (completed !== undefined && completed !== card.completed) {
+    actionList.push('Marked the due date complete')
+  }
+  if (listId !== undefined && listId !== card.listId) {
+    actionList.push('Added this card to my list')
+  }
+  if (archived !== undefined && !archived) {
+    actionList.push('Card was taken out of archive')
+  }
+  if (archived !== undefined && archived) {
+    actionList.push('Card was archived')
+  }
+  if (actionList.length === 0) {
+    return
+  } else {
+    return actionList;
   }
 }
 
